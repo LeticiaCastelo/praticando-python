8ef39192b45d7b0c6ca04b8240fbdf9d01617828
Letícia Castelo <leticiacastelo.dev@gmail.com>
2024-11-13 22:38:25 -0300

    exercícios 2 e 3

---
+++ exercício3.py
@@ -0,0 +1,9 @@
+# Temperatura dos servidores
+
+temperatura_atual = (float(input("Digite a temperatura atual: ")))
+
+if temperatura_atual < 25:
+    print("Temperatura está dentro do limite permitido.")
+
+else:
+    print("Alerta! Temperatura acima do limite permitido.")
